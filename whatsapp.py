9c83eb7042b37232062f79e9604c44bb18245eb4
James <james.sonnino2903@gmail.com>
2025-05-04 00:10:25 +0300

    updated sid and token, and externalized phone numbers

--- whatsapp.py
+++ whatsapp.py
@@ -1,11 +1,14 @@
 from twilio.rest import Client
 from secret import account_sid, auth_token
 
+MY_PERSONAL_WHATSAPP_NUMBER = 'whatsapp:+972547958073' 
+TWILIO_WHATSAPP_NUMBER = 'whatsapp:+14155238886' 
+
 client = Client(account_sid, auth_token)
 
 def send_msg(text):
     client.messages.create(
-        from_ = 'whatsapp:+14155238886',
+        from_ = TWILIO_WHATSAPP_NUMBER,
         body = text,
-        to = 'whatsapp:+972547958073'
+        to = MY_PERSONAL_WHATSAPP_NUMBER
     )
