be540099f6c65348d86a218e67b44ca5db31a805
James <james.sonnino2903@gmail.com>
2025-11-20 13:05:52 +0200

    - moved docker files into project directory - renamed Docker/ to scripts/ - added docker compose file

--- scripts/rebuild_and_run.sh
+++ scripts/rebuild_and_run.sh
@@ -44,7 +44,7 @@ echo -e "\n${GREEN}--- Step 3: Building the new image from project root... ---${
 docker build --no-cache -t $IMAGE_NAME -f "${PROJECT_ROOT}/Docker/Dockerfile" "$PROJECT_ROOT"
 
 echo -e "\n${GREEN}--- Step 4: Starting the new container... ---${NC}"
-docker run -d -p 5000:5000 --env-file "${PROJECT_ROOT}/.env" --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -p 5000:5000 --env-file "${PROJECT_ROOT}/Docker/.env" --name $CONTAINER_NAME $IMAGE_NAME
 
 echo -e "\n${GREEN}--- Process complete! ---${NC}"
 echo "Container '$CONTAINER_NAME' is running with image '$IMAGE_NAME'."
